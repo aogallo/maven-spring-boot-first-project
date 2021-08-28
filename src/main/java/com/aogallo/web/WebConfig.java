fd7a33991781d4a94a5450465b3875daeee295f0
Allan Gallo <allan.gallo.guerra@gmail.com>
2021-08-27 19:05:47 -0600

    Enable security with security-spring

--- src/main/java/com/aogallo/web/WebConfig.java
+++ src/main/java/com/aogallo/web/WebConfig.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.aogallo.web;
 
 import java.util.Locale;
@@ -14,10 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-/**
- *
- * @author allangallo
- */
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
     
